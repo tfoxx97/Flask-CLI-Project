075cf09dfabce167c7ecf28f5997bb9da536ebbf
Tyler Elenberger <tyler.elenberger@gmail.com>
2026-09-24 20:13:12 -0700

    Updated script version to reflect changes to the code.

--- flask_CLI.py
+++ flask_CLI.py
@@ -2,7 +2,7 @@ import cmd, os, subprocess, re
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 available_cmds = {
     'create': "Create a basic folder structure for your flask app.",
